84796b45621cb756db6181da21aed28721728c0b
Kyle Ames <kyle@ameske.org>
2015-04-15 21:03:00 -0400

    Implemented a sticky error reader per the challenge feedback

--- decoder.go
+++ decoder.go
@@ -8,9 +8,38 @@ import (
 	"os"
 )
 
-var MAGIC_NUM = []byte{0x53, 0x50, 0x4c, 0x49, 0x43, 0x45}
+var (
+	MAGIC_NUM = []byte{0x53, 0x50, 0x4c, 0x49, 0x43, 0x45}
+)
 
-var ErrMagicNumberInvalid = errors.New("magic number invalid")
+var (
+	ErrMagicNumberInvalid = errors.New("pattern: magic number invalid")
+)
+
+// StickyErrorReader remembers the first error encountered. Any Read()
+// operations following the first error are effectively no-ops.
+type StickyErrorReader struct {
+	r   io.Reader
+	err error
+}
+
+// Read implements the io.Reader interface on StickyErrorReader. If an error
+// has already occurred it returns that error, otherwise it performs the read.
+func (er *StickyErrorReader) Read(p []byte) (n int, err error) {
+	if er.err != nil {
+		return 0, er.err
+	}
+
+	n, err = er.r.Read(p)
+	er.err = err
+
+	return
+}
+
+// Err returns the first error the StickyErrorReader encountered, if any.
+func (er *StickyErrorReader) Err() error {
+	return er.err
+}
 
 // DecodeFile decodes the drum machine file found at the provided path
 // and returns a pointer to a parsed pattern which is the entry point to the
@@ -28,82 +57,65 @@ func DecodeFile(path string) (*Pattern, error) {
 	return p, err
 }
 
+// A Decoder reads and decodes Pattern files from an input stream
 type Decoder struct {
-	r   io.Reader
-	err error
+	r *StickyErrorReader
 }
 
+// NewDecoder returns a new decoder that reads from r.
 func NewDecoder(r io.Reader) *Decoder {
 	return &Decoder{
-		r:   r,
-		err: nil,
+		r: &StickyErrorReader{r: r},
 	}
 }
 
+// Decode reads a Pattern file from its input and stores it in the value point to by p
 func (dec *Decoder) Decode(p *Pattern) error {
-	if dec.err != nil {
-		return dec.err
-	}
-
+	// Read the header information to verify the input is a pattern file and
+	// learn its size
 	header := struct {
 		Magic    [6]byte
 		FileSize int64
 	}{}
-	err := binary.Read(dec.r, binary.BigEndian, &header)
+	binary.Read(dec.r, binary.BigEndian, &header)
 	if string(header.Magic[:]) != "SPLICE" {
 		return ErrMagicNumberInvalid
 	}
 
-	dec.r = &io.LimitedReader{
+	// We'll now use the limited reader to ensure we only read the one Pattern file
+	r := &io.LimitedReader{
 		R: dec.r,
 		N: header.FileSize,
 	}
 
 	// Version - 32 bytes
 	tmpVersion := new([32]byte)
-	_, err = io.ReadFull(dec.r, tmpVersion[:])
-	if err != nil {
-		return err
-	}
+	io.ReadFull(r, tmpVersion[:])
 	p.Version = string(bytes.Trim(tmpVersion[:], string(0x00)))
 
 	// Tempo - 4 bytes, float32, BigEndian
-	err = binary.Read(dec.r, binary.LittleEndian, &p.Tempo)
-	if err != nil {
-		return err
-	}
+	binary.Read(r, binary.LittleEndian, &p.Tempo)
 
-	// FOR EACH TRACK
+	// Read the tracks associated with this Pattern
 	p.Tracks = make([]*Track, 0)
-	for {
+	for r.N > 0 {
 		t := &Track{}
+
 		// Read the ID (4 bytes)
-		err = binary.Read(dec.r, binary.LittleEndian, &t.ID)
-		if err != nil {
-			break
-		}
+		binary.Read(r, binary.LittleEndian, &t.ID)
 
 		// Read the Len of name (1 byte)
 		nameLen := make([]byte, 1)
-		_, err = io.ReadFull(dec.r, nameLen)
-		if err != nil {
-			break
-		}
+		io.ReadFull(r, nameLen)
 
 		// Read the Name (see prev)
 		tmpName := make([]byte, int(nameLen[0]))
-		_, err = io.ReadFull(dec.r, tmpName)
-		if err != nil {
-			break
-		}
+		io.ReadFull(r, tmpName)
 		t.Name = string(tmpName)
 
 		// Read the steps (16 bytes)
 		tmpSteps := make([]byte, 16)
-		_, err = io.ReadFull(dec.r, tmpSteps)
-		if err != nil {
-			break
-		}
+		io.ReadFull(r, tmpSteps)
 
 		// Convert the step into booleans, we don't care about bytes
 		t.Steps = make([]bool, 16)
@@ -114,13 +126,9 @@ func (dec *Decoder) Decode(p *Pattern) error {
 				t.Steps[i] = false
 			}
 		}
+
 		p.Tracks = append(p.Tracks, t)
 	}
 
-	if err == io.EOF {
-		return nil
-	} else {
-		dec.err = err
-		return err
-	}
+	return dec.r.Err()
 }
